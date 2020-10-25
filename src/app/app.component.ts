e433140d9d44763ece3f2ba78604e9519289b2e5
ananthayeluri <ayeluri001@my.wilmu.edu>
2020-10-25 19:01:47 -0400

    todo

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {Todo} from "./todo";
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hello-world';
+  todoValue:string;
+  list:Todo[];
+  ngOnInit(){
+    this.list = [];
+    this.todoValue = "";
+  }
+  addItem(){
+    if(this.todoValue !== "") {
+      const newItem: Todo = {
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      };
+   this.list.push(newItem);
+    }
+    this.todoValue = ""; //clearing the value
+  }
+  deleteItem(id:number){
+this.list = this.list.filter(item =>item.id !== id);
+  }
+}
+ 
